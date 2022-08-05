41824ce7531b74f9219c7e011b4cceea6ad9479d
penghuo <penghuo@gmail.com>
2022-08-05 15:57:20 -0700

    update

--- core/src/main/java/org/opensearch/sql/planner/logical/LogicalPlanNodeVisitor.java
+++ core/src/main/java/org/opensearch/sql/planner/logical/LogicalPlanNodeVisitor.java
@@ -77,4 +77,8 @@ public abstract class LogicalPlanNodeVisitor<R, C> {
   public R visitAD(LogicalAD plan, C context) {
     return visitNode(plan, context);
   }
+
+  public R visitWrite(LogicalWrite plan, C context) {
+    return visitNode(plan, context);
+  }
 }
---
+++ core/src/main/java/org/opensearch/sql/planner/logical/LogicalWrite.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.opensearch.sql.planner.logical;
+
+import com.google.common.collect.ImmutableList;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+/**
+ * Logical Relation represent the data source.
+ */
+@ToString
+@EqualsAndHashCode(callSuper = true)
+public class LogicalWrite extends LogicalPlan {
+  @Getter
+  private final String relationName;
+
+  /**
+   * Constructor of LogicalRelation.
+   */
+  public LogicalWrite(String relationName) {
+    super(ImmutableList.of());
+    this.relationName = relationName;
+  }
+
+  @Override
+  public <R, C> R accept(LogicalPlanNodeVisitor<R, C> visitor, C context) {
+    return visitor.visitWrite(this, context);
+  }
+}

4391c84f3dbf04d933c34096031b0045c7d067db
Peng Huo <penghuo@gmail.com>
2026-03-08 21:00:10 +0000

    fix(dqe): escape JSON control characters in response
    escapeJson: properly escape all control characters (U+0000 through
    U+001F) as JSON unicode escapes. Fixes jq parse error for SELECT *
    queries where data contains control characters (e.g., BrowserLanguage
    field with non-printable characters).
    
    Fixes Q24 (SELECT * now produces valid JSON response).

--- dqe/src/main/java/org/opensearch/sql/dqe/coordinator/transport/TransportTrinoSqlAction.java
+++ dqe/src/main/java/org/opensearch/sql/dqe/coordinator/transport/TransportTrinoSqlAction.java
@@ -746,11 +746,35 @@ public class TransportTrinoSqlAction
     if (s == null) {
       return "null";
     }
-    return s.replace("\\", "\\\\")
-        .replace("\"", "\\\"")
-        .replace("\n", "\\n")
-        .replace("\r", "\\r")
-        .replace("\t", "\\t");
+    StringBuilder sb = new StringBuilder(s.length() + 16);
+    for (int i = 0; i < s.length(); i++) {
+      char c = s.charAt(i);
+      switch (c) {
+        case '\\':
+          sb.append("\\\\");
+          break;
+        case '"':
+          sb.append("\\\"");
+          break;
+        case '\n':
+          sb.append("\\n");
+          break;
+        case '\r':
+          sb.append("\\r");
+          break;
+        case '\t':
+          sb.append("\\t");
+          break;
+        default:
+          if (c < 0x20) {
+            // Escape control characters as JSON unicode escapes
+            sb.append(String.format("\\u%04x", (int) c));
+          } else {
+            sb.append(c);
+          }
+      }
+    }
+    return sb.toString();
   }
 
   /**
